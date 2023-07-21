dc677f5149b0941e5131a135bccb27f6c42e1eb6
marcelomrad <marcelomrad@gmail.com>
2023-07-21 10:04:52 -0300

    feat: config bd

---
+++ practical_test/src/main/resources/db/migration/V2__Insert_Reminders.sql
@@ -0,0 +1,11 @@
+-- Lembrete 1
+INSERT INTO reminder (name, date) VALUES ('Lembrete 1', '2023-07-21');
+
+-- Lembrete 2
+INSERT INTO reminder (name, date) VALUES ('Lembrete 2', '2023-07-22');
+
+-- Lembrete 3
+INSERT INTO reminder (name, date) VALUES ('Lembrete 3', '2023-07-23');
+
+-- Lembrete 4
+INSERT INTO reminder (name, date) VALUES ('Lembrete 4', '2023-07-24');
